e85cc5c6184808ed7dcd3e920d468d4597bbb786
zoro-2002 <premambre80@gmail.com>
2022-08-08 00:08:07 +0530

    add : added views and the routing to them

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import paperless from "../views/Paperless.vue"
+import Auth from "../views/Auth.vue";
+import Templates from "../views/Templates.vue";
+import Datasets from "../views/Datasets.vue";
+import Dashboard from "../views/Dashboard.vue";
+import Jobs from "../views/Jobs.vue";
+
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "paperless",
-    component: paperless,
-  }
+  { path: '/', redirect: '/auth' },
+  { path: '/auth', component: Auth },
+  { path: '/dashboard', component: Dashboard },
+  { path: '/templates', component: Templates },
+  { path: '/datasets', component: Datasets },
+  { path: '/jobs', component: Jobs }
+
 ];
 
 const router = new VueRouter({
