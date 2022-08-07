65f284172643bb4212f3efdf166f01c52fcc360b
zoro-2002 <premambre80@gmail.com>
2022-08-07 23:49:01 +0530

    init : initialized the store OAuth : added the authentication by email in auth.js module (wip)

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,9 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import auth from "./modules/auth.js";
+import templates from "./modules/templates.js";
+import datasets from "./modules/datasets.js";
+import jobs from "./modules/jobs.js";
 
 Vue.use(Vuex);
 
@@ -7,5 +11,10 @@ export default new Vuex.Store({
   state: {},
   mutations: {},
   actions: {},
-  modules: {},
+  modules: {
+    auth,
+    templates,
+    datasets,
+    jobs
+  },
 });
