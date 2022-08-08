c3c9b5769e1b7702d7bae1dd79d903f2fbec6811
zoro-2002 <premambre80@gmail.com>
2022-08-08 11:45:15 +0530

    add : added paperless header

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
   <v-app>
+    <paperless-header v-show="isAuth"></paperless-header>
     <router-view></router-view>
   </v-app>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+import PaperlessHeader from './components/PaperlessHeader.vue';
 export default {
   name: "App",
-
   data: () => ({
     //
   }),
+  components:{
+    PaperlessHeader
+  },
+  computed: {
+    ...mapGetters('auth', ['isAuth'])
+  }
 };
 </script>
 
