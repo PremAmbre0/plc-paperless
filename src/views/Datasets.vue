c53905fb0c78a12d8bb5a0b3f1dce4441c1c1544
zoro-2002 <premambre80@gmail.com>
2022-08-21 17:41:54 +0530

    add : added ui to the datasets section

--- src/views/Datasets.vue
+++ src/views/Datasets.vue
@@ -1,21 +1,72 @@
 <template>
     <div>
-        Datasets Section
-        <div>
-            {{ data }}
-        </div>
+        <v-container tag="div" fluid>
+            <v-spacer></v-spacer>
+            <v-divider></v-divider>
+            <v-list subheader two-line>
+                <v-list-item v-for="dataset in datasetsList" :key="dataset._id">
+                    <v-list-item-avatar>
+                        <v-icon class="grey lighten-1" dark>
+                            mdi-file-table
+                        </v-icon>
+                    </v-list-item-avatar>
+
+                    <v-list-item-content>
+                        <v-list-item-title v-text="dataset.name"></v-list-item-title>
+
+                        <v-list-item-subtitle v-text="dataset.createdOn"></v-list-item-subtitle>
+                    </v-list-item-content>
+
+                    <v-menu>
+                        <template v-slot:activator="{ on: menu, attrs }">
+                            <v-tooltip bottom z-index="-100">
+                                <template v-slot:activator="{ on: tooltip }">
+                                    <v-btn icon v-bind="attrs" v-on="{ ...tooltip, ...menu }">
+                                        <v-icon>mdi-dots-vertical</v-icon>
+                                    </v-btn>
+                                </template>
+                            </v-tooltip>
+                        </template>
+                        <v-list rounded dense>
+                            <v-list-item v-for="(option, index) in options" :key="index">
+                                <v-list-item-title>{{ option.value }}
+                                </v-list-item-title>
+                            </v-list-item>
+                        </v-list>
+                    </v-menu>
+                </v-list-item>
+            </v-list>
+        </v-container>
+
+        <v-btn class="add" fab dark color="indigo">
+            <v-icon dark>
+                mdi-plus
+            </v-icon>
+        </v-btn>
+        <v-overlay :value="showOverlayLoader">
+            <v-progress-circular indeterminate color="#5243AA" size="70"></v-progress-circular>
+        </v-overlay>
     </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 import { mapActions, mapMutations } from 'vuex'; ``
 
 
 export default {
     data() {
         return {
-            datasetsList: null
+            datasetsList: null,
+            options: [
+                { value: 'View' },
+                { value: 'Delete' },
+                { value: 'Download' },
+            ],
         }
+    },
+    computed: {
+        ...mapGetters(['showOverlayLoader'])
     },
     async created() {
         this.getData();
@@ -40,3 +91,10 @@ export default {
 
 
 </script>
+<style lang="scss" scoped>
+.add {
+    position: fixed;
+    right: 5%;
+    bottom: 3%
+}
+</style>
