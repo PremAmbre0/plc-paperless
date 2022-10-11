7c5a26279698ce8d215a8692854df143107d6f6a
zoro-2002 <premambre80@gmail.com>
2022-10-12 04:39:42 +0530

    feat : added download functionality

--- src/store/index.js
+++ src/store/index.js
@@ -236,7 +236,28 @@ export default new Vuex.Store({
         }
       });
     },
+    downloadFile(context, payload) {
+      axios({
+        url: payload.url,
+        method: 'GET',
+        responseType: 'blob',
+      })
+        .then((response) => {
+          // create file link in browser's memory
+          const href = URL.createObjectURL(response.data);
 
+          // create "a" HTLM element with href to file & click
+          const link = document.createElement('a');
+          link.href = href;
+          link.setAttribute('download', payload.name); //or any other extension
+          document.body.appendChild(link);
+          link.click();
+
+          // clean up "a" element & remove ObjectURL
+          document.body.removeChild(link);
+          URL.revokeObjectURL(href);
+        })
+    },
     // uploadImageToGDrive: async ({ commit, dispatch }, payload) => { },
   },
   getters: {
